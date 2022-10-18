13866ba3caa8a6986aba68989a1c8743661aa537
Christian David Aquino <40892430+Kwaynas@users.noreply.github.com>
2022-10-18 21:45:56 +0800

    v1.1.0.0
    First Deployable update (R2022a Update 3)

--- ReadUID.ino
+++ ReadUID.ino
@@ -1,6 +1,7 @@
-
 #include <SPI.h>
 #include <MFRC522.h>
+#include "LCDIC2.h"
+LCDIC2 lcd(0x27, 16, 2);
 
 #define SS_PIN 10
 #define RST_PIN 9
@@ -13,13 +14,16 @@
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
 MFRC522::MIFARE_Key key; 
-
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC52
+  lcd.begin();
+  lcd.print("Please wait...");
+  delay(2000);
+  lcd.setBacklight(false);
+  lcd.clear(); 
   
 }
 
@@ -32,14 +36,17 @@ void loop() {
   // Verify if the NUID has been readed
   if ( ! rfid.PICC_ReadCardSerial())
     return;
-  // put your main code here, to run repeatedly:
+  // print UID to MATLAB.
     printHex(rfid.uid.uidByte, rfid.uid.size);
-    tone(BZR_PIN, 1500, 250);
+  //wait for matlab  response
+    display();
+  // Delay to prevent double tap
     delay(500);
+  // Insert other functions here
 }
 
 
-
+//UID reading function
 void printHex(byte *buffer, byte bufferSize) {
   for (byte i = 0; i < bufferSize; i++) {
     Serial.print(buffer[i] < 0x10 ? "0" : "");
@@ -48,4 +55,27 @@ void printHex(byte *buffer, byte bufferSize) {
     // Halt PICC
   rfid.PICC_HaltA();
 
+}
+
+void display(){
+  int count = 0;
+  while ((Serial.available() == 0) && (count < 50)){
+    count++;
+    delay(10);
+  }
+  if (Serial.available() > 0){
+    lcd.setBacklight(true);
+    lcd.print("Tap Recorded! :)");
+    tone(BZR_PIN, 1500, 250);
+    delay(2000);
+    lcd.clear();
+    lcd.setBacklight(false);
+    Serial.end();
+    Serial.begin(9600);
+    
+  }
+  else{
+    Serial.print("\tNo input detected\t");
+  }
+  
 }
